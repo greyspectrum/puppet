a506729062d74cbc655c0943a8742cf0901c4ddf
greyspectrum <greyspectrum@users.noreply.github.com>
2017-08-08 13:37:50 -0400

    first commit

---
+++ centos-lamp.pp
@@ -0,0 +1,46 @@
+# execute 'yum update'
+exec { 'yum update':
+  command => '/usr/bin/yum update'
+}
+
+# install httpd  package
+package { 'httpd':
+  require => Exec['yum update'],
+  ensure => installed,
+}
+
+# ensure httpd service is running
+service { 'httpd':
+  ensure => running,
+}
+
+# install mariadb package
+package { 'mariadb-server':
+  require => Exec['yum update'],
+  ensure => installed,
+}
+
+# ensure mariadb service is running
+service { 'mariadb':
+  ensure => running,
+}
+
+# install php
+package { 'php':
+  require => Exec['yum update'],
+  ensure => installed,
+}
+
+# install php-mysql
+package { 'php-mysql':
+  require => Exec['yum update'],
+  ensure => installed,
+}
+
+# ensure info.php file exists
+file { '/var/www/html/info.php':
+  ensure => file,
+  content => '<?php phpinfo(); ?>',
+  require => Package['httpd'],
+}
+
